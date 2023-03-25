4e1f867ecfed88e21c2678903ba6adb9d617ef78
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2023-03-26 00:16:28 +0700

    Add project files.

---
+++ AndroidDeviceHelper/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using AndroidDeviceHelper.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AndroidDeviceHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowResizer = new WindowResizer(this);
+        }
+        private void MoveToTasksPage(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void MoveToSettingPage(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #region Window resizer
+        WindowResizer WindowResizer;
+        // for each rectangle, assign the following method to its PreviewMouseDown event.
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+            e.Handled = true;
+        }
+        private void Resize(object sender, MouseButtonEventArgs e)
+        {
+            WindowResizer.resizeWindow((System.Windows.Controls.Primitives.Thumb)sender);
+            e.Handled = true;
+        }
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
---
+++ AndroidDeviceHelper/Models/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidDeviceHelper.Models
+{
+    public class AppSettings
+    {
+        private static AppSettings? _current = null;
+        public static AppSettings? Current { 
+            set => _current = value;
+            get => _current;
+        }
+        public string AdbPath { get; set; } = null;
+        public DeviceProfile[] DeviceProfiles { get; set; } = new DeviceProfile[0]; 
+    }
+    public class DeviceProfile
+    {
+        public string Name { get; set; } = null;
+        public string Address { get; set; } = null;
+    }
+}
